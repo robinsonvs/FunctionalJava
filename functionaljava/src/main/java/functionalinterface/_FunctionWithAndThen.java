c9a307f8e98bdb9e3e3d994d06f228bd59532575
robinsonvs <robinsonvs@gmail.com>
2020-02-26 14:54:26 -0300

    add BiFunction example and updated other codes

--- functionaljava/src/main/java/functionalinterface/_FunctionWithAndThen.java
+++ functionaljava/src/main/java/functionalinterface/_FunctionWithAndThen.java
@@ -2,7 +2,7 @@ package functionalinterface;
 
 import java.util.function.Function;
 
-public class _Function {
+public class _FunctionWithAndThen {
 
     public static void main(String[] args) {
         int increment = incrementByOne(1);
@@ -10,9 +10,18 @@ public class _Function {
 
         int increment2 = incrementByOneFunction.apply(1);
         System.out.println(increment2);
+
+        int multiply = multiplyBy10Function.apply(increment2);
+        System.out.println(multiply);
+
+        Function<Integer, Integer> addMy1andThenMultiplyBy10 =
+                incrementByOneFunction.andThen(multiplyBy10Function);
+        System.out.println(addMy1andThenMultiplyBy10.apply(4));
     }
 
     static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
+
+    static Function<Integer, Integer> multiplyBy10Function = number -> number * 10;
 
     static int incrementByOne(int number) {
         return number + 1;
---
+++ functionaljava/src/main/java/functionalinterface/_FunctionWithBiFunction.java
@@ -0,0 +1,49 @@
+package functionalinterface;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class _FunctionWithBiFunction {
+
+    public static void main(String[] args) {
+
+        // Function takes 1 argument and produces 1 result
+        int increment = incrementByOne(1);
+        System.out.println(increment);
+
+        int increment2 = incrementByOneFunction.apply(1);
+        System.out.println(increment2);
+
+        int multiply = multiplyBy10Function.apply(increment2);
+        System.out.println(multiply);
+
+        Function<Integer, Integer> addMy1andThenMultiplyBy10 =
+                incrementByOneFunction.andThen(multiplyBy10Function);
+        System.out.println(addMy1andThenMultiplyBy10.apply(4));
+
+        // Function takes 2 arguments and produces 1 result
+        System.out.println(
+                incrementByOneAndMultiply(4, 100)
+        );
+
+        System.out.println(
+                incrementByOneAndMultiplyBiFunction.apply(4, 100)
+        );
+    }
+
+    static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
+
+    static Function<Integer, Integer> multiplyBy10Function = number -> number * 10;
+
+    static int incrementByOne(int number) {
+        return number + 1;
+    }
+
+    static BiFunction<Integer, Integer, Integer> incrementByOneAndMultiplyBiFunction =
+            (numberToIncrementByOne, numberToMultiplyBy)
+                    -> (numberToIncrementByOne + 1) * numberToMultiplyBy;
+
+    static int incrementByOneAndMultiply(int number, int numToMulpliplyBy) {
+        return (number + 1) * numToMulpliplyBy;
+    }
+}
